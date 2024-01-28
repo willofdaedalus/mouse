4adb8d37fe71890741821ea6365fa8f0d510cb1d
Daedalus <manny.is.daedalus@pm.me>
2024-01-28 09:53:21 +0000

    added math operator checks. untested

--- src/interpreter.c
+++ src/interpreter.c
@@ -1,6 +1,7 @@
 #include "interpreter.h"
+#include "stack.h"
 
-#include <string.h>
+#include <stdlib.h>
 #include <stdio.h>
 
 
@@ -12,6 +13,8 @@ void begin_interpreter(char *contents, size_t file_len, stack *stack)
     while (pos < file_len)
     {
         cur_char = contents[pos];
+
+        handle_math_operators(cur_char, &stack);
 
         switch (cur_char)
         {
@@ -42,6 +45,50 @@ void begin_interpreter(char *contents, size_t file_len, stack *stack)
     }
 }
 
-void handle_math_operators(char cur_char, size_t *cur_pos)
+/**
+ * handles math operators that appear while interpreting the file
+ *
+ * @cur_char: the char that is checked
+ * @stack: a pointer to the current stack
+ */
+void handle_math_operators(char cur_char, stack **stack)
 {
+    int lhs = 0, rhs = 0;
+
+    /* aside the commutative properties of * and + the others
+     * need special checks to make sure they function the way
+     * they're supposed to
+     */
+    switch(cur_char)
+    {
+        case '+':
+            rhs = pop(*stack), lhs = pop(*stack);
+            push(*stack, lhs + rhs);
+            break;
+
+        case '*':
+            rhs = pop(*stack), lhs = pop(*stack);
+            push(*stack, lhs * rhs);
+            break;
+
+        case '-':
+            rhs = pop(*stack), lhs = pop(*stack);
+            push(*stack, lhs - rhs);
+            break;
+
+        case '/':
+            rhs = pop(*stack), lhs = pop(*stack);
+
+            /* in case the divisor is 0
+             * we throw a division by zero error
+             */
+            if (rhs == 0)
+            {
+                printf("can't divide by zero\n");
+                exit(EXIT_FAILURE);
+            }
+
+            push(*stack, (int)(lhs / rhs));
+            break;
+    }
 }
--- src/interpreter.h
+++ src/interpreter.h
@@ -6,5 +6,9 @@
 
 void begin_interpreter(char *contents, size_t file_len, stack *stack);
 
+/* handlers */
+void handle_math_operators(char cur_char, stack **stack);
+void handle_operators(char cur_char, stack **stack);
+
 
 #endif
